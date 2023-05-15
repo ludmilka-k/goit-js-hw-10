791fe654895b2f1154894e4abdab7456bc3e3b25
Ludmila Kurochkina <ludmilka.kurochkina@gmail.com>
2023-05-15 22:29:09 +0300

    task search country

--- src/index.js
+++ src/index.js
@@ -1,3 +1,75 @@
 import './css/styles.css';
+import Notiflix from 'notiflix';
+const debounce = require('lodash.debounce');
+import API from './js/fetch-countries.js';
+import getRefs from './js/get-refs.js';
 
 const DEBOUNCE_DELAY = 300;
+
+const refs = getRefs();
+
+refs.searchInput.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
+
+function onSearch(evn) {
+  evn.preventDefault();
+  const searchQuery = evn.target.value.trim();
+  console.log(evn.target.value);
+  clearMarkup();
+
+  if (searchQuery) {
+    API.fetchCountries(searchQuery)
+        .then(makeMarkup)
+        .catch(onFetchError);
+  }
+}
+
+function makeMarkup(countries) {
+  if (countries.length === 1) {
+    renderMarkupItem(countries);
+  } else if (countries.length >= 2 && countries.length <= 10) {
+    renderMarkupList(countries);
+  } else {
+    Notiflix.Notify.info(
+      'Too many matches found. Please enter a more specific name.'
+    );
+  }
+  return;
+}
+
+function renderMarkupList(countries) {
+  const markupList = countries
+    .map(({ name, flags }) => {
+      return `<li class="country-item">
+                <img src="${flags.svg}" width="36" height="24" alt="${name}">
+                <h2 class="country-item-title">${name.official}</h2>
+             </li>`;
+    })
+    .join('');
+  refs.countryList.innerHTML = markupList;
+}
+
+function renderMarkupItem(countries) {
+  const markupItem = countries
+    .map(({ name, flags, capital, population, languages }) => {
+      const languagesList = Object.values(languages).join(', ');
+      return `<div class="country-item-wrap">
+                  <img src="${flags.svg}" width="36" height="24" alt="${name}">
+                  <h2 class="country-item-title">${name.official}</h2>
+               </div>
+               <p><b>Capital:</b>${capital}</p>
+               <p><b>Population:</b>${population}</p>
+               <p><b>Languages:</b>${languagesList}</p>`;
+    })
+    .join('');
+  refs.countryItem.innerHTML = markupItem; //refs.countryItem.insertAdjacentHTML('beforeend', markup);
+}
+
+function clearMarkup() {
+  refs.countryItem.innerHTML = '';
+  refs.countryList.innerHTML = '';
+}
+
+function onFetchError(error) {
+  console.error(error);
+  Notiflix.Notify.failure('Oops, there is no country with that name');
+}
---
+++ src/js/get-refs.js
@@ -0,0 +1,8 @@
+export default function getRefs() {
+    return {
+        searchInput: document.getElementById('search-box'),
+        countryList: document.querySelector('.country-list'),
+        countryItem: document.querySelector('.country-info'),
+    };
+    
+}
