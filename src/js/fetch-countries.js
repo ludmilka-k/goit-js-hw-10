c2c056695a1db5a22d3bf88274390f59774b5528
Ludmila Kurochkina <ludmilka.kurochkina@gmail.com>
2023-05-19 12:24:56 +0300

    update async/await

--- src/js/fetch-countries.js
+++ src/js/fetch-countries.js
@@ -1,12 +1,29 @@
 const BASE_URL = 'https://restcountries.com/v3.1/';
-function fetchCountries(name) {
-  return fetch(`${BASE_URL}/name/${name}?fields=name,capital,population,flags,languages`)
-  .then(response => {
+// function fetchCountries(name) {
+//   return fetch(`${BASE_URL}/name/${name}?fields=name,capital,population,flags,languages`)
+//   .then(response => {
+//     if(!response.ok) {
+//         throw new Error(response.status);
+//     }
+//     return response.json()
+//   })
+//   .catch();
+// }
+
+// export default {fetchCountries}
+
+
+//через async/awaiy
+async function fetchCountries(name) {
+    try {
+    const response = await fetch(`${BASE_URL}/name/${name}?fields=name,capital,population,flags,languages`)
     if(!response.ok) {
         throw new Error(response.status);
     }
-    return response.json()
-  });
+    return await response.json()
+   } catch(error) {
+    console.log(error);
+   }
 }
 
 export default {fetchCountries}
